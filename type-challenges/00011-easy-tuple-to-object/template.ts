8b98dc4ae080d681f07692cbc86e5ee60c872933
Yiling <lynnlai10@hotmail.com>
2022-07-26 21:50:09 +1000

    ease tuple to object

---
+++ type-challenges/00011-easy-tuple-to-object/template.ts
@@ -0,0 +1,24 @@
+type TupleToObject<T extends readonly (string |  number | symbol)[]> = {
+    [P in T[number]]: P
+}
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type t = TupleToObject<typeof tuple>
+
+const tupleToObject = (array: any[]) => {
+    const obj = {};
+    array.map(item => {
+        obj[item] = item;
+    })
+    return obj;
+}
+// index
+type TupleToIndex<T extends readonly any[]> = {
+    [P in keyof T]: P
+}
+
+type r = TupleToIndex<typeof tuple>
+
+// 1. keyof array => index
+// 2. P in T[number] => 遍历数组
